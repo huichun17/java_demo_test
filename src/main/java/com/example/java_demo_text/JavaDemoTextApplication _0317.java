d02dfef7fd05bf47522bae82f9fdf897fa3cbbc8
huichun17 <monday20230220@gmail.com>
2023-03-17 16:00:54 +0800

    branch develop first commit

---
+++ src/main/java/com/example/java_demo_text/JavaDemoTextApplication _0317.java
@@ -0,0 +1,13 @@
+package com.example.java_demo_text;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaDemoTextApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaDemoTextApplication.class, args);
+	}
+
+}
